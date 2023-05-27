e2c5ab25197967042722434e374aaf55f783c5d9
Ahmed Kemala <kemalaahmed@gmail.com>
2023-05-27 18:36:51 +0200

    build: implement storybook

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+module.exports = {
+  stories: [
+    '../stories/*.stories.mdx',
+    '../stories/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  /** Expose public folder to storybook as static */
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+};
